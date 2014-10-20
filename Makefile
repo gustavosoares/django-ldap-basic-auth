15fabafe8e6da7b58d91107acbb0a9a0e9130619
Gustavo Soares Souza <gustavosoares@gmail.com>
2014-10-20 17:09:48 -0200

    adding setup.py and makefile

---
+++ Makefile
@@ -0,0 +1,40 @@
+# Makefile for django-ldap-basic-auth
+
+help:
+	@echo
+	@echo "Please use 'make <target>' where <target> is one of"
+	@echo "	 clean		to clean garbage left by builds and installation"
+	@echo "	 compile	to compile .py files (just to check for syntax errors)"
+	@echo "	 install	to install"
+	@echo "	 build		to build without installing"
+	@echo "	 dist		to create egg for distribution"
+	@echo "	 test		to run tests"
+	@echo "	 publish	to publish the package to PyPI"
+	@echo
+
+clean:
+	@echo "Cleaning..."
+	@rm -rf build dist *.egg-info *.pyc **/*.pyc *~
+
+test: clean
+	@cd testproject; python manage.py test simple_app
+
+compile: clean
+	@echo "Compiling source code..."
+	@python -tt -m compileall simple_audit
+	@python -tt -m compileall tests
+
+install:
+	@python setup.py install
+
+build:
+	@python setup.py build
+
+dist: clean
+	@python setup.py sdist
+
+upload: clean
+	@python setup.py sdist upload -r pypi
+
+publish: upload
+	@bash make_tag.sh
---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+STATUS_PROD = 'Development Status :: 5 - Production/Stable'
+STATUS_BETA = 'Development Status :: 4 - Beta'
+STATUS_ALPHA = 'Development Status :: 3 - Alpha'
+
+version = '0.0.1'
+README = os.path.join(os.path.dirname(__file__), 'README.md')
+long_description = open(README).read()
+setup(
+    name='django-ldap-basic-auth',
+    version=version,
+    description="Simple middleware that injects the Authorization header in the request after authenticating against a ldap server.",
+    long_description=long_description,
+    classifiers=[
+        STATUS_ALPHA,
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'],
+    keywords='revisions versioning history audit',
+    author='Gustavo Soares Souza',
+    author_email='gustavosoares@gmail.com',
+    url='https://github.com/gustavosoares/django-ldap-basic-auth',
+    license='BSD',
+    packages=find_packages('.', exclude=('testproject*',)),
+    include_package_data=True,
+)
